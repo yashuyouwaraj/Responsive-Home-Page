6d7b05eedf0c777d0ea3ee4b32e9334259a90b01
Yashu Youwaraj <amanyasu@gmail.com>
2025-08-27 15:48:13 +0530

    Hero Section Done

--- scripts.js
+++ scripts.js
@@ -2,3 +2,38 @@ const navDialog = document.getElementById('nav-dialog');
 function handleMenu(){
     navDialog.classList.toggle('hidden')
 }
+
+const initialTranslateLTR = -48*4;
+const initialTranslateRTL = 36*4;
+
+function setupIntersectionObserver(element, isLTR, speed){
+    const intersectionCallback = (entries) => {
+        const isIntersection = entries[0].isIntersection;
+        if(isIntersection){
+            document.addEventListener('scroll', scrollHandler);
+        } else{
+            document.addEventListener('scroll', scrollHandler)
+        }
+    }
+    const intersectionObserver = new IntersectionObserver(intersectionCallback);
+    intersectionObserver.observe(element);
+
+    function scrollHandler(){
+        const translateX= (window.innerHeight - element.getBoundingClientRect().top)*speed;
+        let totalTranslate=0;
+        if(isLTR){
+            totalTranslate=translateX+initialTranslateLTR;
+        } else{
+            totalTranslate = -(translateX+initialTranslateRTL);
+        }
+        element.style.transform = `translateX(${totalTranslate}px)`;
+    }
+}
+
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
+const line3 = document.getElementById('line3');
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
